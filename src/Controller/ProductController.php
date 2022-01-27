d62f02535bef967eea61b413e8601cd315a7408f
SUN Julie <sun.julie0@gmail.com>
2022-01-27 19:45:30 +0100

    update

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Classe\Search;
 use App\Entity\Product;
+use App\Form\SearchType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,20 +21,29 @@ class ProductController extends AbstractController
     }
 
     #[Route('/nos-pâtisseries', name: 'products')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
 
-        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        } else {
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
+        }
+
         return $this->render('product/index.html.twig', [
-            'products' => $products
+            'products' => $products,
+            'form' => $form->createView()
         ]);
     }
 
-    #[Route('/pâtisserie/{slug}', name: 'product')]
+    #[Route('/pâtisserie-{slug}', name: 'product')]
     public function show($slug): Response
     {
-        $product = $this->entityManager->getRepository(Product::class)->findOneBy($slug);
+        $product = $this->entityManager->getRepository(Product::class)->findOneBy(['slug' => $slug]);
 
         if (!$product) {
             return $this->redirectToRoute('products');
